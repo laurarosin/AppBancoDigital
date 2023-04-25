191f738c8782f6ce29b472d72ad5f0b4d16d2713
laurarosin <laurarosin29@gmail.com>
2023-04-25 15:47:47 -0300

    Atualizando a DataService

---
+++ AppBancoDigital/AppBancoDigital/View/ChavePixForm.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChavePixForm : ContentPage
+    {
+        public ChavePixForm ()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
