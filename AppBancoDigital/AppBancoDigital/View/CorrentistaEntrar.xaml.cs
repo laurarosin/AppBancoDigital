d7dc5ba3de422137da1a54387a8ba3b2471bae9a
laurarosin <laurarosin29@gmail.com>
2023-05-04 15:52:45 -0300

    Atualizando a pasta View

---
+++ AppBancoDigital/AppBancoDigital/View/CorrentistaEntrar.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CorrentistaEntrar : ContentPage
+    {
+        public CorrentistaEntrar()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_entrar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
