0f32436fbee86b9a1ba49f050406f733810086fc
laurarosin <laurarosin29@gmail.com>
2023-10-05 15:53:22 -0300

    transferencia

--- AppBancoDigital/AppBancoDigital/View/TelaInicial.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/TelaInicial.xaml.cs
@@ -15,6 +15,10 @@ namespace AppBancoDigital.View
         public TelaInicial()
         {
             InitializeComponent();
+            btn_transf.Source = ImageSource.FromResource("AppBancoDigital.Imagem.tranferencias.png");
+            btn_pix.Source = ImageSource.FromResource("AppBancoDigital.Imagem.pix.PNG");
+            btn_pagamentos.Source = ImageSource.FromResource("AppBancoDigital.Imagem.pagamentos.PNG");
+            btn_cartao.Source = ImageSource.FromResource("AppBancoDigital.Imagem.cartao.PNG");
 
             txt_Correntista.Text = App.DadosCorrentistaLogado.Nome;
         }
@@ -30,6 +34,24 @@ namespace AppBancoDigital.View
             Navigation.PushAsync(new View.ReceberPix());
         }
 
-       
+        private void btn_transferencia_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.TelaInicial());
+        }
+
+        private void btn_pix_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.EnviarPix());
+        }
+
+        private void btn_cartao_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.TelaInicial());
+        }
+
+        private void btn_pagamentos_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.TelaInicial());
+        }
     }
 }
